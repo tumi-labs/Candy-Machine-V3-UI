26f56ec0f55f0043f8de100993109ddcc2f87aa2
Ghulam Mustafa Raza <ghulammustafasoomar@gmail.com>
2022-11-03 18:17:33 +0500

    Add Update Candy Machine Logic if it is in cache

--- cmv3-demo-initialization/createCandyMachine.ts
+++ cmv3-demo-initialization/createCandyMachine.ts
@@ -7,13 +7,15 @@ import {
   toDateTime,
   getMerkleRoot,
   token,
+  CandyGuardsSettings,
 } from "@metaplex-foundation/js";
 import { Connection, clusterApiUrl, Keypair } from "@solana/web3.js";
 import { writeFileSync } from "fs";
 
 (async function () {
   const cache = require("./cache.json");
-  if (cache.program) return console.log('Program already found in your cache, exiting...');
+  // if (cache.program)
+  //   return console.log("Program already found in your cache, exiting...");
 
   const allowList = require("./allowlist.json");
   const demoNftCollection = new PublicKey(
@@ -47,126 +49,160 @@ import { writeFileSync } from "fs";
           updateAuthority: key,
         })
       ).nft.address;
-
+  const createOrUpdateCandyMachine = async (
+    config: CandyGuardsSettings & any,
+    {
+      candyMachine,
+      candyGuard,
+    }: { candyMachine?: string; candyGuard?: string } = {}
+  ): Promise<{ candyMachine: PublicKey; candyGuard?: PublicKey }> => {
+    if (candyMachine) {
+      // await metaplex.candyMachines().update({
+      //   candyMachine: new PublicKey(candyMachine),
+      //   ...config,
+      // });
+      if (candyGuard) {
+        await metaplex.candyMachines().updateCandyGuard({
+          candyGuard: new PublicKey(candyGuard),
+          ...config,
+        });
+      }
+      return {
+        candyMachine: new PublicKey(candyMachine),
+        candyGuard: candyGuard && new PublicKey(candyGuard),
+      };
+    } else {
+      return metaplex
+        .candyMachines()
+        .create(config)
+        .then(({ candyMachine }) => ({
+          candyMachine: candyMachine.address,
+          candyGuard: candyMachine.candyGuard?.address,
+        }));
+    }
+  };
   // Create the Candy Machine.
-  const { candyMachine } = await metaplex.candyMachines().create({
-    ...config,
-    itemsAvailable: toBigNumber(number),
-    collection: {
-      address: collectionMint,
-      updateAuthority: key,
-    },
-    guards: {
-      botTax: {
-        lamports: sol(0.1337),
-        lastInstruction: true,
+  const { candyMachine, candyGuard } = await createOrUpdateCandyMachine(
+    {
+      ...config,
+      itemsAvailable: toBigNumber(number),
+      collection: {
+        address: collectionMint,
+        updateAuthority: key,
       },
-      startDate: {
-        date: toDateTime("2022-10-20 18:00:00 +0000"),
-      },
-      endTime: {
-        date: toDateTime("2022-10-20 18:00:00 +0000"),
-      },
-    },
-    groups: [
-      {
-        label: "public", // Public (Mint Limit[1], Redeemed Amount[50])
-        guards: {
-          mintLimit: {
-            id: 1,
-            limit: 1,
-          },
-          redeemedAmount: {
-            maximum: toBigNumber(50),
-          },
+      guards: {
+        botTax: {
+          lamports: sol(0.1337),
+          lastInstruction: true,
+        },
+        startDate: {
+          date: toDateTime("2022-10-20 18:00:00 +0000"),
+        },
+        endTime: {
+          date: toDateTime("2022-10-20 18:00:00 +0000"),
         },
       },
-      // {
-      //   label: "owner", // Owner (Address Gate)
-      //   guards: {
-      //     addressGate: {
-      //       address: demoDestination,
-      //     },
-      //   },
-      // },
-      {
-        label: "waoed", // Whitelist (Allowlist)
-        guards: {
-          allowList: {
-            merkleRoot: getMerkleRoot(allowList),
+      groups: [
+        {
+          label: "public", // Public (Mint Limit[1], Redeemed Amount[50])
+          guards: {
+            mintLimit: {
+              id: 1,
+              limit: 1,
+            },
+            redeemedAmount: {
+              maximum: toBigNumber(50),
+            },
           },
         },
-      },
-      // {
-      //   label: "nftBrn", // Breading NFT (NFT Burn)
-      //   guards: {
-      //     nftBurn: {
-      //       requiredCollection: demoNftCollection,
-      //     },
-      //   },
-      // },
-      {
-        label: "nftGte", // OGs Mint (NFT Gate)
-        guards: {
-          nftGate: {
-            requiredCollection: demoNftCollection,
+        // {
+        //   label: "owner", // Owner (Address Gate)
+        //   guards: {
+        //     addressGate: {
+        //       address: demoDestination,
+        //     },
+        //   },
+        // },
+        {
+          label: "waoed", // Whitelist (Allowlist)
+          guards: {
+            allowList: {
+              merkleRoot: getMerkleRoot(allowList),
+            },
           },
         },
-      },
-      {
-        label: "nftPmt", // Swap NFT (NFT Payment)
-        guards: {
-          nftPayment: {
-            requiredCollection: demoNftCollection,
-            destination: demoDestination,
+        // {
+        //   label: "nftBrn", // Breading NFT (NFT Burn)
+        //   guards: {
+        //     nftBurn: {
+        //       requiredCollection: demoNftCollection,
+        //     },
+        //   },
+        // },
+        {
+          label: "nftGte", // OGs Mint (NFT Gate)
+          guards: {
+            nftGate: {
+              requiredCollection: demoNftCollection,
+            },
           },
         },
-      },
-      {
-        label: "solPmt", // Premium (Sol Payment)
-        guards: {
-          solPayment: {
-            amount: sol(0.1),
-            destination: demoDestination,
+        {
+          label: "nftPmt", // Swap NFT (NFT Payment)
+          guards: {
+            nftPayment: {
+              requiredCollection: demoNftCollection,
+              destination: demoDestination,
+            },
           },
         },
-      },
-      {
-        label: "tknBrn", // Token Burn
-        guards: {
-          tokenBurn: {
-            amount: token(1),
-            mint: demoTokenMint,
+        {
+          label: "solPmt", // Premium (Sol Payment)
+          guards: {
+            solPayment: {
+              amount: sol(0.1),
+              destination: demoDestination,
+            },
           },
         },
-      },
-      {
-        label: "tknGte", // Token Gate
-        guards: {
-          tokenGate: {
-            amount: token(1),
-            mint: demoTokenMint,
-          },
-        },
-      },
-      {
-        label: "tknPmt", // Token Payment
-        guards: {
-          tokenPayment: {
-            amount: token(1),
-            mint: demoTokenMint,
-            destinationAta: metaplex.tokens().pdas().associatedTokenAccount({
+        {
+          label: "tknBrn", // Token Burn
+          guards: {
+            tokenBurn: {
+              amount: token(1, 9),
               mint: demoTokenMint,
-              owner: demoDestination,
-            }),
+            },
           },
         },
-      },
-    ],
-  });
+        {
+          label: "tknGte", // Token Gate
+          guards: {
+            tokenGate: {
+              amount: token(1, 9),
+              mint: demoTokenMint,
+            },
+          },
+        },
+        {
+          label: "tknPmt", // Token Payment
+          guards: {
+            tokenPayment: {
+              amount: token(1, 9),
+              mint: demoTokenMint,
+              destinationAta: metaplex.tokens().pdas().associatedTokenAccount({
+                mint: demoTokenMint,
+                owner: demoDestination,
+              }),
+            },
+          },
+        },
+      ],
+    },
+    cache.program || {}
+  );
   cache.program = {
-    candyMachine: candyMachine.address.toString(),
-    candyGuard: candyMachine.candyGuard?.address.toString(),
+    candyMachine: candyMachine.toString(),
+    candyGuard: candyGuard.toString(),
     candyMachineCreator: key.publicKey.toString(),
     collectionMint: collectionMint.toString(),
   };
