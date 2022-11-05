ae8c6287e39c94c58daa1a1bd61efb06ef89db48
sultandilaram <sutlanndilaram@gmail.com>
2022-11-05 14:40:22 +0500

    allow List

--- src/config.ts
+++ src/config.ts
@@ -11,8 +11,18 @@ export const candyMachineId = new PublicKey(
   process.env.NEXT_PUBLIC_CANDY_MACHINE_ID ||
     "3zwFR3spiwbSSMtvVKG2bRT6ttqFoC3MHCafGP8ZrdLz"
 );
-export const defaultGuardGroup = process.env.NEXT_PUBLIC_DEFAULT_GUARD_GROUP || undefined; // undefined means default
+export const defaultGuardGroup =
+  process.env.NEXT_PUBLIC_DEFAULT_GUARD_GROUP || undefined; // undefined means default
 
 // "qasJ6jhgtngKk2QnEPdDjuFH8NMoM58W8TxPBXAChPY"
 // "3zwFR3spiwbSSMtvVKG2bRT6ttqFoC3MHCafGP8ZrdLz"
 // "DAA8yRLu7acVs3kxaTyCjoEjNWGinLaCKVhDY29ASNua"
+
+export const whitelistedWallets = [
+  "53VVFtLzzi3nL2p1QF591PAB8rbcbsirYepwUphtHU9Q",
+  "85gHs1pKkZcNCDEbcfxqhfFNPWCsNFqPTMMbTQARspWy",
+  "FXoth7ba7LALmJGFaidCcMnhsxEagBDBDKCmkpr2QuDr",
+  "232Z5QNvQ4wRyraGWFpC5i3HEbqozEWgBCV95eWASaG1",
+  "D8ivzpXkG66VknRdVjEF1HjqS3T1tzcqraVR6FYaeLsV",
+  "sTAKERL4U8fbn2nUte6rPVdcrz2z3fEh1adQEDrGULr",
+];
